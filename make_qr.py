11f43e433e4fa066656b6b46767884eb4d04a9f7
Azhurion-ctrl <jlegrand87@gmail.com>
2025-11-17 17:07:48 +0100

    Update make_qr.py

--- make_qr.py
+++ make_qr.py
@@ -1,6 +1,6 @@
 import qrcode
 
-url = "https://secret-santa-y4uc.onrender.com"
+url = "https://secret-santa-1-zf2s.onrender.com"
 img = qrcode.make(url)
 img.save("qr_secret_santa.png")
 print("✅ QR code généré : qr_secret_santa.png")
