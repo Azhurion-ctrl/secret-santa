b7c8b3ba82fff7b70b767c65c3e8f2d514657624
Azhurion-ctrl <jlegrand87@gmail.com>
2025-11-17 17:38:06 +0100

    Update admin.py

--- admin.py
+++ admin.py
@@ -30,7 +30,7 @@ def admin():
     return render_template("admin.html", participants=participants)
 
 if __name__ == "__main__":
-   import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
  
